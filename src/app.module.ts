ce82d7f7d282cc3b0b7a1f00f6c56f36ede25289
snowykte0426 <snowykte0426@naver.com>
2025-01-14 20:32:15 +0900

    feat: RDB 연결 설정 추가
    RDB와 연결 정보를 추가하였습니다
    
    Resolves: #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                type: 'mysql',
+                host: configService.get<string>('DB_HOST', 'localhost'),
+                port: configService.get<number>('DB_PORT', 3306),
+                username: configService.get<string>('DB_USER', 'root'),
+                password: configService.get<string>('DB_PASSWORD', ''),
+                database: configService.get<string>('DB_NAME', 'test'),
+                synchronize: false,
+            }),
+        })
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
