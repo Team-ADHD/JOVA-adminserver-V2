9747fe3ed1fc5ca8b967861af5000d0e38ff42f4
snowykte0426 <snowykte0426@naver.com>
2025-01-16 23:29:01 +0900

    feat: ``alarm`` Entity 필드 자료형 객체 정의
    사용자 알람 정보를 저장하는 테이블의 칼럼 중 하나의 형식을 정의하는 클래스를 선언하였습니다
    
    Resolves: #2

---
+++ src/alarm/dto/alarm.content.ts
@@ -0,0 +1,13 @@
+import {Column} from "typeorm";
+import {AlarmLevelEnum} from "../enums/alarm.level.enum";
+
+export class AlarmContent {
+    @Column({nullable: false})
+    title: string;
+    @Column({nullable: false})
+    content: string;
+    @Column({type: 'enum', enum: AlarmLevelEnum, nullable: false})
+    level: AlarmLevelEnum;
+    @Column({name: 'created_at', type: 'timestamp', nullable: false})
+    createdAt: Date;
+}
