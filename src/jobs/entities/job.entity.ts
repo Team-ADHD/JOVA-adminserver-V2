ffcc9dff7d85584f69d8a04c63c841c0acf1f73c
snowykte0426 <snowykte0426@naver.com>
2025-01-17 23:34:44 +0900

    feat: ``job`` Entity 정의
    구인 글 데이터를 저장하는 테이블의 Entity를 정의하고 필요한 클래스 역시 동시에 구현하였습니다
    
    Resolves: #2

---
+++ src/jobs/entities/job.entity.ts
@@ -0,0 +1,43 @@
+import {
+    Column,
+    CreateDateColumn, Entity, JoinColumn,
+    JoinTable,
+    ManyToMany, ManyToOne,
+    OneToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from "typeorm";
+import {MajorEntity} from "../../majors/entities/major.entity";
+import {Application} from "../../applications/entities/application.entity";
+import {JobStatusEnum} from "../enums/job.status.enum";
+import {UserEntity} from "../../users/entities/user.entity";
+
+@Entity('jobs')
+export class JobEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @ManyToOne(() => UserEntity, {nullable: false, eager: false})
+    @JoinColumn({name: 'user_id'})
+    user: UserEntity;
+    @Column({length: 100, nullable: false})
+    title: string;
+    @Column({name:"content",type: 'text', nullable: false})
+    description: string;
+    @Column({name:"deadline",type: 'timestamp', nullable: false})
+    closingDate: Date;
+    @Column({type: 'enum', enum: JobStatusEnum, nullable: false})
+    status: JobStatusEnum;
+    @OneToMany(() => Application, (application) => application.job)
+    applications: Application[];
+    @ManyToMany(() => MajorEntity)
+    @JoinTable({
+        name: 'job_required_majors',
+        joinColumn: {name: 'job_id', referencedColumnName: 'id'},
+        inverseJoinColumn: {name: 'major_id', referencedColumnName: 'id'},
+    })
+    requiredMajors: MajorEntity[];
+    @CreateDateColumn({name: 'created_at'})
+    createdAt: Date;
+    @UpdateDateColumn({ name: 'updated_at'})
+    updatedAt: Date;
+}
