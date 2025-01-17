856842f9c70d6b2c08385cbc3595e18deb398bf6
snowykte0426 <snowykte0426@naver.com>
2025-01-17 23:35:06 +0900

    feat: ``major`` Entity 정의
    전공 데이터를 저장하는 테이블의 Entity를 정의하였습니다
    
    Resolves: #2

---
+++ src/majors/entities/major.entity.ts
@@ -0,0 +1,9 @@
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity('majors')
+export class MajorEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column({length: 20, unique: true, nullable: false})
+    name: string;
+}
