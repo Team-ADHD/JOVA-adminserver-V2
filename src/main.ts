1b036b10a6d2b4f0472044ee12eac0022e865664
snowykte0426 <snowykte0426@naver.com>
2025-01-17 23:37:50 +0900

    feat: 사용자 친화적 반환값 추가 및 인증 파이프라인 재정의
    Nest.js의 유효성 검증 파이프라인 설정을 변경하였고 쿼리 실패 시 클라이언트에 단순히 500를 반환하는 것이 아닌 상황에 따라 다른 응답을 제공하도록 수정하였습니다
    
    Resolves: #2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {ValidationPipe} from "@nestjs/common";
+import {QueryFailedExceptionFilter} from "./exceptions/handler/query-failed-error.filter";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            forbidNonWhitelisted: true,
+            whitelist: true,
+        }),
+    )
+    app.useGlobalFilters(new QueryFailedExceptionFilter());
+    await app.listen(process.env.PORT ?? 3001);
 }
+
 bootstrap();
