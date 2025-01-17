b71355f1051cd5a5aac988c4947db82f15718b32
snowykte0426 <snowykte0426@naver.com>
2025-01-17 23:31:20 +0900

    feat: 암호화 클래스 및 예외 처리 클래스 포함
    애플리케이션에 암호화를 담당하는 클래스와 각종 사용자 지정된 예외를 처리하는 클래스를 포함하였습니다
    
    Resolves: #2

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,10 @@ import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {ConfigModule, ConfigService} from "@nestjs/config";
 import {TypeOrmModule} from "@nestjs/typeorm";
+import {AdminModule} from './admin/admin.module';
+import {APP_FILTER} from "@nestjs/core";
+import {BaseExceptionFilter} from "./exceptions/base-exception.filter";
+import {SecurityBcryptService} from "./security/security.bcrypt.service";
 
 @Module({
     imports: [
@@ -19,12 +23,22 @@ import {TypeOrmModule} from "@nestjs/typeorm";
                 username: configService.get<string>('DB_USER', 'root'),
                 password: configService.get<string>('DB_PASSWORD', ''),
                 database: configService.get<string>('DB_NAME', 'test'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}'],
                 synchronize: false,
             }),
-        })
+        }),
+        AdminModule
     ],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [
+        {
+            provide: APP_FILTER,
+            useClass: BaseExceptionFilter,
+        },
+        SecurityBcryptService,
+        AppService
+    ],
+    exports: [SecurityBcryptService]
 })
 export class AppModule {
 }
