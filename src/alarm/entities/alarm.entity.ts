b4c9ea68aadd65e6d2a79bd873a75b352b04b08c
snowykte0426 <snowykte0426@naver.com>
2025-01-17 23:30:24 +0900

    feat: ``alarm`` Entity 정의
    사용자 알람 정보를 저장하는 Entity를 정의하였습니다
    
    Resolves: #2

---
+++ src/alarm/entities/alarm.entity.ts
@@ -0,0 +1,17 @@
+import {Entity, PrimaryGeneratedColumn, OneToOne, JoinColumn, Column} from "typeorm";
+import {UserEntity} from "../../users/entities/user.entity";
+import {AlarmStatusEnum} from "../enums/alarm.status.enum";
+import {AlarmContent} from "../dto/alarm.content";
+
+@Entity('alarms')
+export class Alarm {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @OneToOne(() => UserEntity)
+    @JoinColumn({name: 'user_id'})
+    user: UserEntity;
+    @Column({name:"alarm_status",type: 'enum', enum: AlarmStatusEnum, nullable: false})
+    alarmStatus: AlarmStatusEnum;
+    @Column(() => AlarmContent)
+    alarmContent: AlarmContent;
+}
