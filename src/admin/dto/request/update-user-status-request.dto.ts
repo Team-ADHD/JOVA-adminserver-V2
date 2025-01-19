d6ca55e8e6763089d7477409b1df8bba2e4fa6b4
snowykte0426 <snowykte0426@naver.com>
2025-01-20 02:13:55 +0900

    feat: 필드 추가 및 Request DTO 추가 선언
    사용자 계정의 상태를 변경하는 API의 Request Body 형식을 강제하는 DTO와 사용자 정보 갱신 후 반환되는 Reponse Body DTO에 필드를 추가하였습니다
    
    Resolves: #2

---
+++ src/admin/dto/request/update-user-status-request.dto.ts
@@ -0,0 +1,7 @@
+import {IsBoolean, IsNotEmpty} from "class-validator";
+
+export class UpdateUserStatusRequestDto {
+    @IsNotEmpty({message: 'Banned status must be provided'})
+    @IsBoolean({message: 'Banned status must be a boolean'})
+    banned: boolean;
+}
--- src/admin/dto/response/update-user-response.dto.ts
+++ src/admin/dto/response/update-user-response.dto.ts
@@ -8,8 +8,9 @@ export class UpdateUserResponseDto {
     classNum: number;
     generation: number;
     profilePictureUri: string | null;
+    banned: boolean;
 
-    constructor(id: number, email: string, role: UserRoleEnum, grade: number, classNum: number, generation: number, profilePictureUri: string | null) {
+    constructor(id: number, email: string, role: UserRoleEnum, grade: number, classNum: number, generation: number, profilePictureUri: string | null, banned: boolean) {
         this.id = id;
         this.email = email;
         this.role = role;
@@ -17,5 +18,6 @@ export class UpdateUserResponseDto {
         this.classNum = classNum;
         this.generation = generation;
         this.profilePictureUri = profilePictureUri;
+        this.banned = banned;
     }
 }
