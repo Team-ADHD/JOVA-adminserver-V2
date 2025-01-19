9f49784568a580d88ead5f09ac3628a98cfeeb4b
snowykte0426 <snowykte0426@naver.com>
2025-01-19 13:11:28 +0900

    refactor: 값 검증 데코레이터 및 Entity 전환 메서드 추가
    값의 최대,최소값이나 문자열임을 검증하는 데코레이터를 추가하였고 DTO을 Entity로 전환하는 메서드 역시 추가하였습니다
    
    Resolves: #2

--- src/admin/dto/request/update-user-request.dto.ts
+++ src/admin/dto/request/update-user-request.dto.ts
@@ -1,11 +1,41 @@
 import {UserRoleEnum} from "../../../users/enums/user.role.enum";
+import {IsEmail, IsEnum, IsInt, IsNotEmpty, IsString, Max, MaxLength, Min, MinLength} from "class-validator";
+import {UserEntity} from "../../../users/entities/user.entity";
 
 export class UpdateUserRequestDto {
+    @IsString({message: 'Email must be a string'})
+    @IsEmail({}, {message: 'Invalid email address'})
     email?: string;
+    @IsString({message: 'Password must be a string'})
+    @MinLength(6, {message: 'Password must be at least 6 characters long'})
+    @MaxLength(100, {message: 'Password must be at most 100 characters long'})
     password?: string;
+    @IsEnum(UserRoleEnum, {message: 'Invalid role provided'})
     role?: UserRoleEnum;
+    @IsInt({message: 'Grade must be an integer'})
+    @Max(4, {message: 'Grade must be at most 4'})
+    @Min(1, {message: 'Grade must be at least 1'})
     grade?: number;
+    @IsInt({message: 'Class number must be an integer'})
+    @Max(4, {message: 'Class number must be at most 4'})
+    @Min(1, {message: 'Class number must be at least 1'})
     classNum?: number;
+    @IsInt({message: 'Generation must be an integer'})
+    @Max(9, {message: 'Generation must be at most 9'})
+    @Min(1, {message: 'Generation must be at least 1'})
     generation?: number;
+    @IsString({message: 'Profile picture URI must be a string'})
     profilePictureUri?: string | null;
+
+    toEntity(): UserEntity {
+        const user = new UserEntity();
+        if (this.email) user.email = this.email;
+        if (this.password) user.password = this.password;
+        if (this.role) user.role = this.role;
+        if (this.grade) user.grade = this.grade;
+        if (this.classNum) user.classNum = this.classNum;
+        if (this.generation) user.generation = this.generation;
+        if (this.profilePictureUri) user.profilePictureUri = this.profilePictureUri;
+        return user;
+    }
 }
